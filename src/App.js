f092c6ec0df5fe6f40ad46a7b8fd5fce0a8f1d46
nishitatsu-dev <tatsuhiko.nishi@gmail.com>
2023-10-26 06:12:52 +0900

    idとnextIdのスコープを適正化の為、定義位置を使用関数内に移動

--- src/App.js
+++ src/App.js
@@ -10,8 +10,6 @@ export default function App() {
     localStorage.setItem("memoApp", JSON.stringify(memos));
   }, [memos]);
 
-  const ids = memos.map((memo) => memo.id);
-  const nextId = ids.length === 0 ? 1 : Math.max(...ids) + 1;
   const allMemos = memos.map((memo) => (
     <li key={memo.id}>
       <span
@@ -29,6 +27,8 @@ export default function App() {
   }
 
   function handleAddMemo() {
+    const ids = memos.map((memo) => memo.id);
+    const nextId = ids.length === 0 ? 1 : Math.max(...ids) + 1;
     setMemos([...memos, { id: nextId, title: "新規メモ", body: "" }]);
     setActiveId(nextId);
   }
